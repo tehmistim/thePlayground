cf16813ebd58817a485ed759f4569416d3a3d0fd
Tim Washington <taw@tehmwashington.com>
2022-01-14 19:45:43 -0600

    01-14 night push

--- navigation.js
+++ navigation.js
@@ -4,6 +4,7 @@ import React from 'react'
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
 import NewPostScreen from './screens/NewPostScreen'
+import SignupScreen from './screens/SignupScreen'
 
 
 const Stack = createStackNavigator()
@@ -16,13 +17,14 @@ const SignedInStack = () => (
 
     <NavigationContainer>
         <Stack.Navigator 
-            initialRouteName='LoginScreen' 
+            initialRouteName='SignupScreen' 
             screenOptions={screenOptions}
         >
             <Stack.Screen name='HomeScreen' component={HomeScreen} /> 
             <Stack.Screen name='NewPostScreen' component={NewPostScreen} />
             <Stack.Screen name='LoginScreen' component={LoginScreen} /> 
- 
+            <Stack.Screen name='SignupScreen' component={SignupScreen} /> 
+
         </Stack.Navigator>
     </NavigationContainer>
     
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { StyleSheet, View, Image } from 'react-native'
-import LoginForm from '../components/loginScreen/LoginForm';
 
 const PLAYGROUND_LOGO = 'https://scontent-atl3-1.xx.fbcdn.net/v/t39.30808-6/271935955_10226154154629216_1263521107427513589_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=ZuYF48Cnr6YAX_z8HrG&_nc_ht=scontent-atl3-1.xx&oh=00_AT-HufQnqC-7sBBBiW1AEet_Yalc81t6ov6-3TvqIcwTSA&oe=61E5F138'
 
 
-const LoginScreen = () => (
+const SignupScreen = () => (
     <View style={ styles.container }>
         <View style={ styles.logoContainer }>
             <Image source={{ uri: PLAYGROUND_LOGO, height: 100, width: 350 }} />
         </View>
-        <LoginForm />
+        {/* <SignupForm /> */}
     </View>
 )
 
@@ -31,4 +30,4 @@ const styles = StyleSheet.create ({
 
 })
 
-export default LoginScreen
+export default SignupScreen
