b7c6b45ce1cb9b3cdae42eca8cb817ba05e4975e
Tim Washington <taw@tehmwashington.com>
2022-01-16 18:32:45 -0600

    01-16 push

---
+++ components/signupScreen/SignupForm.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import Validator from 'email-validator';
+
+
+const SignupForm = () => {
+    const SignupFormSchema = Yup.object().shape({
+        email: Yup.string().email().required('Email is required'),
+        password: Yup.string()
+            .required()
+            .min(8, 'Password must be at least 8 characters in length')
+
+    });
+        
+    return (
+            <View style={styles.wrapper}>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={values => {
+                        console.log(values)
+                    }}
+                    validationSchema={LoginFormSchema}
+                    validateOnMount={true}
+                >
+                   {({ handleChange, handleBlur, handleSubmit, values, isValid }) => (
+                <>
+                   
+                <View style={[ styles.inputField, 
+                    {
+                        borderColor: values.email.length < 1 || Validator.validate(values.email) ? '#ccc' : 'red'
+                    },
+                ]}>
+                    <TextInput
+                        placeholderTextColor='#444'
+                        placeholder='Phone number, username or email'
+                        autoCapitalize='none'
+                        keyboardType='email-address'
+                        textContentType='emailAddress'
+                        autoFocus={true}
+                        onChangeText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        value={values.email}
+                    />
+                </View>
+
+                <View style={[ styles.inputField, 
+                    {
+                        borderColor: 1 > values.password.length || values.password.length >= 8 ? '#ccc' : 'red',
+                    },
+                ]}>
+                    <TextInput 
+                        placeholderTextColor='#444'
+                        placeholder='Password'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        secureTextEntry={true}
+                        textContentType='password'
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        value={values.password}
+                    />
+                </View>
+                <View style={{ alignItems: 'flex-end', marginBottom: 30 }}>
+                    <Text style={{ color: '#6BB0F5'}}>Forgot Password?</Text>
+                </View>
+                <Pressable 
+                    titleSize={20} 
+                    style={styles.button} 
+                    onPress={ handleSubmit }
+                >
+                    <Text style={styles.buttonText}>Login</Text>
+                </Pressable>
+
+                <View style={styles.signupContainer}>
+                    <Text>
+                        Don't have an account yet?
+                    </Text>
+                    <TouchableOpacity>
+                        <Text style={{ color: '#6BB0F5' }}>          Sign Up Now</Text>
+                    </TouchableOpacity>
+                </View>
+                </>
+                   )}
+                </Formik>
+            
+    
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: 80,
+    },
+
+    inputField: {
+        borderRadius: 4,
+        padding: 12,
+        backgroundColor: '#c3bcaa',
+        TextInput: 'white',
+        marginBottom: 10,
+        borderWidth: 1,
+    },
+
+    button: {
+        backgroundColor: '#0096F6',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: 42,
+        borderRadius: 4,
+    },
+
+    buttonText: {
+        fontWeight: '600',
+        color: '#fff',
+        fontSize: 18,
+    },
+
+    signupContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'center',
+        marginTop: 50,
+    }
+})
+
+export default SignupForm
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native';
+import SignUpForm from '../components/signupScreen/SignupForm';
 
 const PLAYGROUND_LOGO = 'https://scontent-atl3-1.xx.fbcdn.net/v/t39.30808-6/271935955_10226154154629216_1263521107427513589_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=ZuYF48Cnr6YAX_z8HrG&_nc_ht=scontent-atl3-1.xx&oh=00_AT-HufQnqC-7sBBBiW1AEet_Yalc81t6ov6-3TvqIcwTSA&oe=61E5F138'
 
@@ -9,7 +10,7 @@ const SignupScreen = () => (
         <View style={ styles.logoContainer }>
             <Image source={{ uri: PLAYGROUND_LOGO, height: 100, width: 350 }} />
         </View>
-        {/* <SignupForm /> */}
+        {/* <SignUpForm /> */}
     </View>
 )
 
